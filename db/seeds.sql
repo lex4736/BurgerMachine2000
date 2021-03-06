2c66fdc42f962136b93f8305b1457f8c84c48403
Alexis Arando <lex4736@gmail.com>
2021-03-06 04:35:49 -0800

    Setup config by adding connection and orm scripts

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('cheese');
+INSERT INTO burgers (burger_name) VALUES ('hamburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('burger3', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('burger4', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('burger5', true);
+INSERT INTO burgers (burger_name) VALUES ('Oburger6');
