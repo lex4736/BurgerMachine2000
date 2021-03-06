9cc0ebd7f0f7632fbbd5f82e74ea888d66cacfe8
Alexis Arando <lex4736@gmail.com>
2021-03-06 04:32:41 -0800

    Added necessary javascript files to folder structure

---
+++ config/orm.js
@@ -0,0 +1,37 @@
+const connection = require('./connection.js');
+
+const orm = {
+    selectAll(tableInput, cb) {
+      const queryString = `SELECT * FROM ${tableInput};`;
+      connection.query(queryString, (err, result) => {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    insertOne(tableInput, objects, cb) {
+        const queryString = `INSERT INTO ${tableInput} SET ?`;
+        connection.query(queryString, objects, (err, result) => {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+      },
+
+      updateOne(tableInput, array, cb) {
+        console.log(array); 
+        const queryString = `UPDATE ${tableInput} SET ? WHERE ?`;
+        // write an array an object 
+     
+
+        connection.query(queryString, array, (err, result) => {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+      },
+}
+module.exports = orm;
