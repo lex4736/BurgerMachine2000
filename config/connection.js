93c3c23bbe3cd72237932cd24afe6d60b1423cbf
Alexis Arando <lex4736@gmail.com>
2021-04-05 15:07:04 -0700

    test

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql');
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: 'password',
-  database: 'burgers_db',
+  database: 'burgers_db'
 });
-
+}
 // Make connection.
 connection.connect((err) => {
   if (err) {
