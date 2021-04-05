de585c2a474fd604f778d6c366bdf1f1d6d68f1e
Alexis Arando <lex4736@gmail.com>
2021-04-05 14:56:40 -0700

    fixing error

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
