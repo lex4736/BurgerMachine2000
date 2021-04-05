deaaa7ffa9aea55330b69c37e375dda985a81a52
Alexis Arando <lex4736@gmail.com>
2021-04-05 14:59:38 -0700

    test

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
@@ -22,4 +22,3 @@ connection.connect((err) => {
 
 // Export connection for our ORM to use.
 module.exports = connection;
-
